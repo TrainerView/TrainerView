86366633553bdca12b689623fb5f1e79d6f7b8ef
Nikita <n.shvedov27@gmail.com>
2024-10-13 10:57:53 +0500

    add ui tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,10 @@ dependencies {
 
     implementation("com.google.android.material:material:1.11.0")
 //    implementation("com.google.android.material:material:1.13.0")
+
+    androidTestImplementation("androidx.test.ext:junit-ktx:1.1.0")
+
+    androidTestImplementation("com.kaspersky.android-components:kaspresso:1.5.4")
+    androidTestImplementation("com.kaspersky.android-components:kaspresso-allure-support:1.5.4")
+    androidTestImplementation("com.kaspersky.android-components:kaspresso-compose-support:1.5.4")
 }
---
+++ app/src/androidTest/java/com/trainerview/app/SimpleActivityTest.kt
@@ -0,0 +1,19 @@
+package com.trainerview.app
+
+import androidx.test.ext.junit.rules.activityScenarioRule
+import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import org.junit.Rule
+import org.junit.Test
+
+class SimpleActivityTest : TestCase() {
+
+    @get:Rule
+    val activityRule = activityScenarioRule<MainActivity>()
+
+    @Test
+    fun test() {
+        MainScreen {
+            createGroupBtn.isVisible()
+        }
+    }
+}
